e6a703fed6d672f91fd24789c04c56ffe171e127
Sebastian Arbutina <sarbutina20@gmail.com>
2024-01-13 21:52:06 +0100

    3. commit

--- app/src/main/java/com/example/airdevops/MainActivity.kt
+++ app/src/main/java/com/example/airdevops/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             AiRDevOpsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Promjena radi commita")
                 }
             }
         }
